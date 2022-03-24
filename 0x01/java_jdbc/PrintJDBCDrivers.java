508aecccd11c9c914ca5442c0480c1627c80cd5e
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-03-24 12:40:03 -0300

    task 0

--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -1,31 +1,29 @@
 import java.sql.Driver;
 import java.sql.DriverManager;
-import java.util.Collections;
-import java.util.List;
+import java.util.Enumeration;
 
-public class PrintJDBCDrivers{
-
-
-    public static void printDriver(Driver driver) {
-
-        String nome = driver.getClass().getName();
-        int majorVersion = driver.getMajorVersion();
-        int minorVersion = driver.getMinorVersion();
-
-        System.out.println("Driver Name: " + nome);
-        System.out.println("Driver Major Version: " + majorVersion);
-        System.out.println("Driver Driver Minor: " + minorVersion);
+    public class PrintJDBCDrivers{
 
 
-    }
-
-    public static void main(String[] args) {
-        for (Enumeration<Driver> element = DriverManager.getDrivers(); element.hasMoreElements();) {
-            Driver driver = element.nextElement();
-            printDriver(driver);
-        }
-
-    }
-            
+        public static void getDriver(Driver d) {
+    
+            String nome = d.getClass().getName();
+            int majorVersion = d.getMajorVersion();
+            int minorVersion = d.getMinorVersion();
+    
+            System.out.println("Driver Name: " + nome);
+            System.out.println("Driver Major Version: " + majorVersion);
+            System.out.println("Driver Driver Minor: " + minorVersion);
+    
+    
         }
     
+        public static void main(String[] args) {
+            for (Enumeration<Driver> element = DriverManager.getDrivers(); element.hasMoreElements();) {
+                Driver driver = element.nextElement();
+                getDriver(driver);
+            }
+    
+        }
+    
+    }
